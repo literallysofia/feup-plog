4f89ef29c26bd8641d08638ac6e05ccce3533c1c
Sofia Silva <up201505628@fe.up.pt>
2017-11-05 19:59:31 +0000

    menu feito, organizei as cenas tambem

---
+++ Project_1/src/menus.pl
@@ -0,0 +1,58 @@
+mainMenu :-
+    printMainMenu,
+    askMenuOption,
+    read(Input),
+    manageInput(Input).
+
+manageInput(1) :-
+    write('valid option!\n\n').
+
+manageInput(2) :-
+    write('valid option!\n\n').
+
+manageInput(3) :-
+    write('valid option!\n\n').
+
+manageInput(4) :-
+    write('valid option!\n\n').
+
+manageInput(5) :-
+    write('\nExiting...\n\n').
+
+manageInput(_Other) :-
+    write('\nERROR: that option does not exist.\n\n'),
+    askMenuOption,
+    read(Input),
+    manageInput(Input).
+
+printMainMenu :-
+    nl,nl,
+    write(' _______________________________________________________________________ '),nl,
+    write('|                                                                       |'),nl,
+    write('|                                                                       |'),nl,
+    write('|                         ___     _       _ _                           |'),nl,
+    write('|                        |  _|___| |_ ___|_| |_                         |'),nl,
+    write('|                        |  _| ."| . |  _| | "_|                        |'),nl,
+    write('|                        |_| |__,|___|_| |_|_,_|                        |'),nl,
+    write('|                                                                       |'),nl,
+    write('|                                                                       |'),nl,
+    write('|                           Barbara Sofia Silva                         |'),nl,
+    write('|                              Julieta Frade                            |'),nl,
+    write('|               -----------------------------------------               |'),nl,
+    write('|                                                                       |'),nl,
+    write('|                                                                       |'),nl,
+    write('|                          1. Player vs Player                          |'),nl,
+    write('|                                                                       |'),nl,
+    write('|                          2. Player vs Computer                        |'),nl,
+    write('|                                                                       |'),nl,
+	write('|                          3. Computer vs Computer                      |'),nl,
+	write('|                                                                       |'),nl,
+    write('|                          4. How to Play                               |'),nl,
+    write('|                                                                       |'),nl,
+    write('|                          5. Exit                                      |'),nl,
+    write('|                                                                       |'),nl,
+    write('|                                                                       |'),nl,
+    write(' _______________________________________________________________________ '),nl,nl,nl.
+
+askMenuOption :-
+    write('> Insert your option: ').
