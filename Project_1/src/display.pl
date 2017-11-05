5c8d8693fa4be28163e49c9dccdfa769fe114570
Sofia Silva <up201505628@fe.up.pt>
2017-11-05 17:35:47 +0000

    minor fix

--- Project_1/src/display.pl
+++ Project_1/src/display.pl
@@ -18,29 +18,29 @@ symbol(black,S) :- S='X'.
 symbol(white,S) :- S='O'.
 symbol(red,S) :- S='*'.
 
-letter(1, L) :-L='A'.
-letter(2, L) :-L='B'.
-letter(3, L) :-L='C'.
-letter(4, L) :-L='D'.
-letter(5, L) :-L='E'.
-letter(6, L) :-L='F'.
-letter(7, L) :-L='G'.
-letter(8, L) :-L='H'.
-letter(9, L) :-L='I'.
-letter(10, L) :-L='J'.
-letter(11, L) :-L='K'.
+letter(1, L) :- L='A'.
+letter(2, L) :- L='B'.
+letter(3, L) :- L='C'.
+letter(4, L) :- L='D'.
+letter(5, L) :- L='E'.
+letter(6, L) :- L='F'.
+letter(7, L) :- L='G'.
+letter(8, L) :- L='H'.
+letter(9, L) :- L='I'.
+letter(10, L) :- L='J'.
+letter(11, L) :- L='K'.
 
-number('A', N) :-N=1.
-number('B', N) :-N=2.
-number('C', N) :-N=3.
-number('D', N) :-N=4.
-number('E', N) :-N=5.
-number('F', N) :-N=6.
-number('G', N) :-N=7.
-number('H', N) :-N=8.
-number('I', N) :-N=9.
-number('J', N) :-N=10.
-number('K', N) :-N=11.
+number('A', N) :- N=1.
+number('B', N) :- N=2.
+number('C', N) :- N=3.
+number('D', N) :- N=4.
+number('E', N) :- N=5.
+number('F', N) :- N=6.
+number('G', N) :- N=7.
+number('H', N) :- N=8.
+number('I', N) :- N=9.
+number('J', N) :- N=10.
+number('K', N) :- N=11.
 
 /*number(_, _) :-
     write('Invalid input\n').*/
@@ -48,22 +48,23 @@ number('K', N) :-N=11.
 printBoard(X) :-
     write('  | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10| 11|\n'),
     write('--|---|---|---|---|---|---|---|---|---|---|---|\n'),
-    printMatrix(X,1).
+    printMatrix(X, 1).
 
-printMatrix([],12).
-printMatrix([Head|Tail],N) :-
-    letter(N,L),
+printMatrix([], 12).
+
+printMatrix([Head|Tail], N) :-
+    letter(N, L),
     write(L),
-    N1 is N+1,
+    N1 is N + 1,
     write(' | '),
     printLine(Head),
     nl,
-    printMatrix(Tail,N1).
+    printMatrix(Tail, N1).
 
 printLine([]).
+
 printLine([Head|Tail]) :-
-    symbol(Head,S),
+    symbol(Head, S),
     write(S),
     write(' | '),
     printLine(Tail).
-
--- Project_1/src/fabrik.pl
+++ Project_1/src/fabrik.pl
@@ -2,66 +2,67 @@
 
 
 replaceInList([_|T], 0, Value, [Value|T]).
-replaceInList([H|T], Index, Value, [H|TNew]):-
+replaceInList([H|T], Index, Value, [H|TNew]) :-
         Index > 0,
-        Index1 is Index-1,
-        replaceInList(T,Index1, Value, TNew).
+        Index1 is Index - 1,
+        replaceInList(T, Index1, Value, TNew).
 
-replaceInMatrix([H|T], 0, Collumn,Value, [HNew|T]):-
-        replaceInList(H,Collumn,Value,HNew).
+replaceInMatrix([H|T], 0, Collumn,Value, [HNew|T]) :-
+        replaceInList(H, Collumn, Value, HNew).
+
 replaceInMatrix([H|T], Row, Collumn, Value, [H|TNew]):-
         Row > 0,
-        Row1 is Row-1,
+        Row1 is Row - 1,
         replaceInMatrix(T, Row1, Collumn, Value, TNew).
 
-askCoords(Board,Player,NewBoard):-
+askCoords(Board, Player, NewBoard) :-
         write('  > Row     '),
         read(RowLetter),
         number(RowLetter, Row),
         write('  > Collumn '),
         read(Collumn),
         write('\n'),
-        CollumnIndex is Collumn-1,
-        RowIndex is Row-1,
+        CollumnIndex is Collumn - 1,
+        RowIndex is Row - 1,
         replaceInMatrix(Board, RowIndex, CollumnIndex, Player, NewBoard).
 
-moveWorker(Board,'Y', NewBoard) :-
+moveWorker(Board, 'Y', NewBoard) :-
         write('\n2. Choose worker new cell.\n'),
-        askCoords(Board,red,NewBoard),
+        askCoords(Board, red, NewBoard),
         printBoard(NewBoard),
         write('\n3. Choose your cell.\n').
 
-moveWorker(Board,'N',NewBoard) :-
+moveWorker(Board, 'N',NewBoard) :-
         NewBoard = Board,
         write('\n2. Choose your cell.\n').
 
-gameLoop(Board1):-
+gameLoop(Board1) :-
       write('\n------------------ PLAYER X -------------------\n\n'),
       write('1. Do you want to move a worker?(Y/N) '),
       read(MoveWorkerBoolX),
-      moveWorker(Board1,MoveWorkerBoolX,Board2),
-      askCoords(Board2,black, Board3),
+      moveWorker(Board1, MoveWorkerBoolX, Board2),
+      askCoords(Board2, black, Board3),
       printBoard(Board3),
       write('\n------------------ PLAYER O -------------------\n\n'),
       write('1. Do you want to move a worker?(Y/N) '),
       read(MoveWorkerBoolO),
-      moveWorker(Board3,MoveWorkerBoolO,Board4),
-      askCoords(Board4,white,Board5),
+      moveWorker(Board3, MoveWorkerBoolO, Board4),
+      askCoords(Board4, white, Board5),
       printBoard(Board5),
       gameLoop(Board5).
 
-addWorkers(InitialBoard,WorkersBoard):-
+addWorkers(InitialBoard, WorkersBoard) :-
       printBoard(InitialBoard),
       write('\n------------------ PLAYER X -------------------\n\n'),
       write('1. Choose worker 1 cell.\n'),
-      askCoords( InitialBoard,red, Worker1Board),
+      askCoords(InitialBoard, red, Worker1Board),
       printBoard(Worker1Board),
       write('\n------------------ PLAYER O -------------------\n\n'),
       write('1. Choose worker 2 cell.\n'),
-      askCoords( Worker1Board,red,WorkersBoard),
+      askCoords(Worker1Board, red, WorkersBoard),
       printBoard(WorkersBoard).
 
-fabrik:-
+fabrik :-
       initialBoard(InitialBoard),
-      addWorkers(InitialBoard,WorkersBoard),
+      addWorkers(InitialBoard, WorkersBoard),
       gameLoop(WorkersBoard).
