641499a61c4967af8c1ad41da11412787504cbcd
julietafrade97 <up201506530@fe.up.pt>
2017-11-10 12:47:42 +0000

    organizing

--- Project_1/src/logic.pl
+++ Project_1/src/logic.pl
@@ -3,110 +3,85 @@ invalidInput(Board, Player, NewBoard, Expected) :-
       write('INVALID INPUT: Cell not valid, please try again.\n'),
       askCoords(Board, Player, NewBoard, Expected).
 
-%O
-verifyLine(_Board, _WorkerRow, _WorkerColumn, _Row, _Column, 12, Res, 'O') :-
+%Default
+verifyLine(_Board, _WorkerRow, _WorkerColumn, _Row, _Column, 12, Res, _Ray) :-
       Res is 0.
+%O
 verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,Index, Res, 'O' ):-
       (Column =:= WorkerColumn - Index, Row =:= WorkerRow, Res is 1);
-      ((WorkerColumnTemp is WorkerColumn - Index, getValueFromMatrix(Board, WorkerRow, WorkerColumnTemp, Value), Value \= empty, Res is 0),!);
+      ((ColumnTemp is WorkerColumn - Index, getValueFromMatrix(Board, WorkerRow, ColumnTemp, Value), Value \= empty, Res is 0),!);
       (Index < 12,
       Index1 is Index + 1,
       verifyLine(Board, WorkerRow, WorkerColumn, Row, Column, Index1, Res, 'O')).
-
 %NO
-verifyLine(_Board, _WorkerRow, _WorkerColumn, _Row, _Column, 12, Res, 'NO') :-
-      Res is 0.
 verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,Index, Res, 'NO' ):-
       (Row =:= WorkerRow - Index, Column =:= WorkerColumn - Index, Res is 1); %NO
-      ((WorkerRowTemp is WorkerRow - Index, WorkerColumnTemp is WorkerColumn - Index, getValueFromMatrix(Board, WorkerRowTemp, WorkerColumnTemp, Value), Value \= empty, Res is 0),!);
+      ((RowTemp is WorkerRow - Index, ColumnTemp is WorkerColumn - Index, getValueFromMatrix(Board, RowTemp, ColumnTemp, Value), Value \= empty, Res is 0),!);
       (Index < 12,
       Index1 is Index + 1,
       verifyLine(Board, WorkerRow, WorkerColumn, Row, Column, Index1, Res, 'NO')).
-
 %N
-verifyLine(_Board, _WorkerRow, _WorkerColumn, _Row, _Column, 12, Res, 'N') :-
-      Res is 0.
 verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,Index, Res, 'N' ):-
       (Row =:= WorkerRow - Index, Column =:= WorkerColumn, Res is 1);
-      ((WorkerRowTemp is WorkerRow - Index, getValueFromMatrix(Board, WorkerRowTemp, WorkerColumn, Value), Value \= empty, Res is 0),!);
+      ((RowTemp is WorkerRow - Index, getValueFromMatrix(Board, RowTemp, WorkerColumn, Value), Value \= empty, Res is 0),!);
       (Index < 12,
       Index1 is Index + 1,
       verifyLine(Board, WorkerRow, WorkerColumn, Row, Column, Index1, Res, 'N')).
-
 %NE
-verifyLine(_Board, _WorkerRow, _WorkerColumn, _Row, _Column, 12, Res, 'NE') :-
-      Res is 0.
 verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,Index, Res, 'NE' ):-
       (Row =:= WorkerRow - Index, Column =:= WorkerColumn + Index, Res is 1);
-      ((WorkerRowTemp is WorkerRow -Index, WorkerColumnTemp is WorkerColumn + Index, getValueFromMatrix(Board, WorkerRowTemp, WorkerColumnTemp, Value), Value \= empty, Res is 0),!);
+      ((RowTemp is WorkerRow -Index, ColumnTemp is WorkerColumn + Index, getValueFromMatrix(Board, RowTemp, ColumnTemp, Value), Value \= empty, Res is 0),!);
       (Index < 12,
       Index1 is Index + 1,
       verifyLine(Board, WorkerRow, WorkerColumn, Row, Column, Index1, Res, 'NE')).
-
 %E
-verifyLine(_Board, _WorkerRow, _WorkerColumn, _Row, _Column, 12, Res, 'E') :-
-      Res is 0.
 verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,Index, Res, 'E' ):-
       (Column =:= WorkerColumn + Index, Row =:= WorkerRow, Res is 1);
-      ((WorkerColumnTemp is WorkerColumn + Index, getValueFromMatrix(Board, WorkerRow, WorkerColumnTemp, Value), Value \= empty, Res is 0),!);
+      ((ColumnTemp is WorkerColumn + Index, getValueFromMatrix(Board, WorkerRow, ColumnTemp, Value), Value \= empty, Res is 0),!);
       (Index < 12,
       Index1 is Index + 1,
       verifyLine(Board, WorkerRow, WorkerColumn, Row, Column, Index1, Res, 'E')).
-
 %SE
-verifyLine(_Board, _WorkerRow, _WorkerColumn, _Row, _Column, 12, Res, 'SE') :-
-      Res is 0.
 verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,Index, Res, 'SE' ):-
       (Row =:= WorkerRow + Index, Column =:= WorkerColumn + Index, Res is 1);
-      ((WorkerRowTemp is WorkerRow + Index, WorkerColumnTemp is WorkerColumn + Index, getValueFromMatrix(Board, WorkerRowTemp, WorkerColumnTemp, Value), Value \= empty, Res is 0),!);
+      ((RowTemp is WorkerRow + Index, ColumnTemp is WorkerColumn + Index, getValueFromMatrix(Board, RowTemp, ColumnTemp, Value), Value \= empty, Res is 0),!);
       (Index < 12,
       Index1 is Index + 1,
       verifyLine(Board, WorkerRow, WorkerColumn, Row, Column, Index1, Res, 'SE')).
-
 %S
-verifyLine(_Board, _WorkerRow, _WorkerColumn, _Row, _Column, 12, Res, 'S') :-
-      Res is 0.
 verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,Index, Res, 'S'):-
       (Row =:= WorkerRow + Index, Column =:= WorkerColumn, Res is 1);
-      ((WorkerRowTemp is WorkerRow + Index, getValueFromMatrix(Board, WorkerRowTemp, WorkerColumn, Value), Value \= empty, Res is 0),!);
+      ((RowTemp is WorkerRow + Index, getValueFromMatrix(Board, RowTemp, WorkerColumn, Value), Value \= empty, Res is 0),!);
       (Index < 12,
       Index1 is Index + 1,
       verifyLine(Board, WorkerRow, WorkerColumn, Row, Column, Index1, Res, 'S')).
-
 %SO
-verifyLine(_Board, _WorkerRow, _WorkerColumn, _Row, _Column, 12, Res, 'SO') :-
-      Res is 0.
 verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,Index, Res, 'SO' ):-
       (Row =:= WorkerRow + Index, Column =:= WorkerColumn - Index, Res is 1);
-      ((WorkerRowTemp is WorkerRow + Index, WorkerColumnTemp is WorkerColumn - Index, getValueFromMatrix(Board, WorkerRowTemp, WorkerColumnTemp, Value), Value \= empty, Res is 0),!);
+      ((RowTemp is WorkerRow + Index, ColumnTemp is WorkerColumn - Index, getValueFromMatrix(Board, RowTemp, ColumnTemp, Value), Value \= empty, Res is 0),!);
       (Index < 12,
       Index1 is Index + 1,
       verifyLine(Board, WorkerRow, WorkerColumn, Row, Column, Index1, Res, 'SO')).
 
-
 %Res = 1 if that cell is in the worker lines, Res = 0 if it's not.
 isWorkerLines(Board, WorkerRow, WorkerColumn, Row, Column, Res) :-
-      (verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,1, ResN, 'N' ), ResN =:= 1, write('\nRes N '),write(ResN),Res is 1);
-      (verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,1, ResNE, 'NE' ), ResNE =:= 1, write('\nRes NE '),write(ResNE),Res is 1);
-      (verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,1, ResE, 'E' ), ResE =:= 1, write('\nRes E '),write(ResE),Res is 1);
-      (verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,1, ResSE, 'SE' ), ResSE =:= 1, write('\nRes SE '),write(ResSE),Res is 1);
-      (verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,1, ResS, 'S' ), ResS =:= 1, write('\nRes S '),write(ResS),Res is 1);
-      (verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,1, ResSO, 'SO' ), ResSO =:= 1, write('\nRes SO '),write(ResSO),Res is 1);
-      (verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,1, ResO, 'O' ), ResO =:= 1, write('\nRes O '),write(ResO),Res is 1);
-      (verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,1, ResNO, 'NO' ), ResNO =:= 1, write('\nRes NO '),write(ResNO),Res is 1);
+      (verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,1, ResN, 'N' ), ResN =:= 1, Res is 1);
+      (verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,1, ResNE, 'NE' ), ResNE =:= 1, Res is 1);
+      (verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,1, ResE, 'E' ), ResE =:= 1, Res is 1);
+      (verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,1, ResSE, 'SE' ), ResSE =:= 1, Res is 1);
+      (verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,1, ResS, 'S' ), ResS =:= 1, Res is 1);
+      (verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,1, ResSO, 'SO' ), ResSO =:= 1, Res is 1);
+      (verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,1, ResO, 'O' ), ResO =:= 1, Res is 1);
+      (verifyLine(Board, WorkerRow, WorkerColumn, Row, Column,1, ResNO, 'NO' ), ResNO =:= 1, Res is 1);
       Res is 0.
 
 
 %Res = 1 that cell is valid, Res = 0 if not.
 isValidPosLines(Board, Row, Column, Res) :-
       getWorkersPos(Board, Worker1Row, Worker1Column, Worker2Row, Worker2Column),
-      (
-        (isWorkerLines(Board, Worker1Row, Worker1Column, Row, Column, 1, ResIsWorkerLines1), ResIsWorkerLines1 =:= 1,
-        Res is 1);
-        (isWorkerLines(Board, Worker2Row, Worker2Column, Row, Column, 1, ResIsWorkerLines2), ResIsWorkerLines2 =:= 1,
-        Res is 1);
-        Res is 0
-      ).
+      ((isWorkerLines(Board, Worker1Row, Worker1Column, Row, Column, ResIsWorkerLines1), ResIsWorkerLines1 =:= 1, Res is 1);
+       (isWorkerLines(Board, Worker2Row, Worker2Column, Row, Column, ResIsWorkerLines2), ResIsWorkerLines2 =:= 1, Res is 1);
+       Res is 0).
 
 askCoords(Board, Player, NewBoard, Expected) :-
         manageRow(_Row, NewRow),
