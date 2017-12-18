bc4dae06e4be8fdd3ecc80cf08ca08fde6749829
julietafrade97 <up201506530@fe.up.pt>
2017-12-18 16:16:38 +0000

    acho que com isto ja nao temos zero yupiii

--- Projeto2/distribuicao_publico.pl
+++ Projeto2/distribuicao_publico.pl
@@ -1,26 +1,51 @@
 :- use_module(library(clpfd)).
 :- use_module(library(lists)).
 
-
-%TODO: parser do input com Count
-
-test(Input, Output, Distances, TotalDistance):-
-    length(Output, 8),
-    domain(Output, 0 , 2),
-    global_cardinality(Output,[0-4, 1-2, 2-2]),
+%TODO:PARSER DE INPUT
+test(Input, Output, Distances, TotalDistance, TotalDifference, Total):-
+    length(Output, 8), %Harcoded
+    domain(Output, 0 , 2), %Hardcoded
+    global_cardinality(Output,[0-4, 1-2, 2-2]), %Hardcoded
     fill_distances(Output, Distances),
     sum(Distances,#=,TotalDistance),
-    labeling([minimize(TotalDistance)], Output).
+    fill_differences(Input,Output, Differences),
+    sum(Differences,#=,TotalDifference),
+    Total#= TotalDifference + TotalDistance,
+    labeling([],Output).
 
-%não da por causa do backtracking mas com ! deixa de dar em cima
-%also nao posso dar zero quando nao ha proximo porue estou a dar prioridade à situaçao
-get_distance([], 0, _, _).
-get_distance([Value|_], Counter, Counter, Value).
-get_distance([_|ListT], Distance, Counter, Value):-
-    NewCounter is Counter+1,
-    get_distance(ListT, Distance, NewCounter, Value).
+fill_differences([],[],[]).
+fill_differences([InputH|InputT], [OutputH|OutputT], [DifferencesH|DifferencesT]):-
+    InputH #= OutputH #<=> DifferencesH #= 0,
+    InputH #\= OutputH #<=> DifferencesH #= 1,
+    fill_differences(InputT, OutputT, DifferencesT).
+
+%BUG: isto conta a distancia dos ultimos ate ao final
+get_distance(Counter, NotUnique, Vars, Value) :-
+    distance_signature(Vars, Sign, Value),
+    automaton(Sign,_,Sign,
+        [source(i), sink(i), sink(j)],
+        [arc(i,0,i, [C+1,  NU+0]), arc(i,1,j, [C+1, NU+1]),
+        arc(j,0,j, [C+0,  NU+0]), arc(j,1,j, [C+0,  NU+0])],
+        %BUG: Se o C começar a 0, a funçao fill_distance dá no,
+        [C, NU], [1,0], [Counter,NotUnique]).    
+
+distance_signature([],[], _).
+distance_signature([X|Xs], [S|Ss], Value):-
+    S in 0..1,
+    X#\=Value #<=> S#=0,
+    X#=Value #<=> S#=1,
+    distance_signature(Xs,Ss, Value).
 
 fill_distances([],[]).
 fill_distances([ListH|ListT], [DistanceH|DistanceT]):-
-    get_distance(ListT, DistanceH, 1, ListH),
-    fill_distances(ListT, DistanceT).
+    get_distance(AuxDistanceH, NotUnique, ListT, ListH),
+    NotUnique #<=> DistanceH #=AuxDistanceH,
+    #\NotUnique #<=> DistanceH #= 0,
+    fill_distances(ListT,DistanceT).
+
+
+
+
+
+
+
