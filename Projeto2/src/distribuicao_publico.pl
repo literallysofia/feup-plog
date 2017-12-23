d44063edcaaa3485c3718df136f9ef19caca02ae
Sofia Silva <up201505628@fe.up.pt>
2017-12-23 17:17:13 +0000

    report: visualização :ballot_box_with_check:

--- Projeto2/src/distribuicao_publico.pl
+++ Projeto2/src/distribuicao_publico.pl
@@ -2,41 +2,41 @@
 :- use_module(library(lists)).
 :- use_module(library(random)).
 
-displayOutput(OutputGroups, OutputIndexs, TotalDifference, NumOfChanges):-
-    write(' > OUTPUT GROUPS: '), write(OutputGroups),nl,
-    write(' > OUTPUT INDEXS: '),write(OutputIndexs),nl,
-    write(' > Total Changes: '), write(NumOfChanges),nl,
-    write(' > Total Changes Value: '), write(TotalDifference),nl.
+displayOutput(OutputGroups, OutputIndexs, TotalDifference, NumOfChanges) :-
+    write(' > OUTPUT GROUPS: '), write(OutputGroups), nl,
+    write(' > OUTPUT INDEXS: '), write(OutputIndexs), nl,
+    write(' > Total Changes: '), write(NumOfChanges), nl,
+    write(' > Total Changes Value: '), write(TotalDifference), nl.
 
 generateList(0, [], _).
 generateList(Counter, [Head|Tail], TotalGroups) :-
     Counter > 0,
-    Counter1 is Counter-1,
+    Counter1 is Counter - 1,
     random(1, TotalGroups, Head),
     generateList(Counter1, Tail, TotalGroups).
 
-problem(TotalAudience, TotalGroups):-
+problem(TotalAudience, TotalGroups) :-
     MaxGroups is TotalGroups + 1,
     generateList(TotalAudience, InputGroups, MaxGroups),
-    write(' > INPUT  GROUPS:'), write(InputGroups),nl,
-    solve(InputGroups, TotalAudience, TotalGroups, OutputGroups, OutputIndexs, TotalDifference,  NumOfChanges),
+    write(' > INPUT  GROUPS:'), write(InputGroups), nl,
+    solve(InputGroups, TotalAudience, TotalGroups, OutputGroups, OutputIndexs, TotalDifference, NumOfChanges),
     displayOutput(OutputGroups, OutputIndexs, TotalDifference, NumOfChanges).
 
-problem(InputGroups):-
+problem(InputGroups) :-
     length(InputGroups, TotalAudience),
     maximum(TotalGroups, InputGroups),
-    solve(InputGroups, TotalAudience, TotalGroups, OutputGroups, OutputIndexs, TotalDifference,  NumOfChanges),
-    write(' > INPUT  GROUPS: '), write(InputGroups),nl,
+    solve(InputGroups, TotalAudience, TotalGroups, OutputGroups, OutputIndexs, TotalDifference, NumOfChanges),
+    write(' > INPUT  GROUPS: '), write(InputGroups), nl,
     displayOutput(OutputGroups, OutputIndexs, TotalDifference, NumOfChanges).
 
-solve(InputGroups, TotalAudience, TotalGroups, OutputGroups, OutputIndexs, TotalDifference, NumOfChanges):-
+solve(InputGroups, TotalAudience, TotalGroups, OutputGroups, OutputIndexs, TotalDifference, NumOfChanges) :-
     statistics(walltime, [Start,_]),
 
     %Variáveis de Decisão
     length(OutputGroups, TotalAudience),
     length(OutputIndexs, TotalAudience),
-    domain(OutputGroups, 1 , TotalGroups),
-    domain(OutputIndexs, 1 , TotalAudience), 
+    domain(OutputGroups, 1, TotalGroups),
+    domain(OutputIndexs, 1, TotalAudience), 
     
     %Restrições
     all_distinct(OutputIndexs),
@@ -44,14 +44,14 @@ solve(InputGroups, TotalAudience, TotalGroups, OutputGroups, OutputIndexs, Total
     approximate(OutputGroups),
 
     %Função de Avaliação
-    fill_differences(OutputIndexs,OutputIndexs, Differences),
-    sum(Differences,#=,TotalDifference),
+    fill_differences(OutputIndexs, OutputIndexs, Differences),
+    sum(Differences, #=, TotalDifference),
     get_changes(NumOfChanges, Differences),
     Min #= NumOfChanges + TotalDifference,
 
     %Labelling
     append(OutputGroups, OutputIndexs, Vars),
-    labeling([minimize(Min),step,min],Vars),
+    labeling([minimize(Min), step, min], Vars),
 
     statistics(walltime, [End,_]),
 	Time is End - Start,
@@ -59,13 +59,13 @@ solve(InputGroups, TotalAudience, TotalGroups, OutputGroups, OutputIndexs, Total
     %fd_statistics.
 
 get_groups(_,[],[]).
-get_groups(InputGroups, [OutputIndexsH|OutputIndexsT],  [OutputGroupsH|OutputGroupsT]):-
+get_groups(InputGroups, [OutputIndexsH|OutputIndexsT], [OutputGroupsH|OutputGroupsT]):-
     element(OutputIndexsH, InputGroups, OutputGroupsH),
     get_groups(InputGroups, OutputIndexsT, OutputGroupsT).
 
 get_distance(Counter, NotUnique, OutputGroupsT, Value) :-
     distance_signature(OutputGroupsT, Sign, Value),
-    automaton(Sign,_,Sign,
+    automaton(Sign, _, Sign,
         [source(i), sink(i), sink(j)],
         [arc(i,0,i, [C+1,  NU+0]), arc(i,1,j, [C+0, NU+1]),
         arc(j,0,j, [C+0,  NU+0]), arc(j,1,j, [C+0,  NU+0])],
@@ -76,29 +76,29 @@ distance_signature([X|Xs], [S|Ss], Value):-
     S in 0..1,
     X#\=Value #=> S#=0,
     X#=Value #=> S#=1,
-    distance_signature(Xs,Ss, Value).
+    distance_signature(Xs, Ss, Value).
 
 approximate([]).
-approximate([OutputGroupsH|OutputGroupsT]):-
+approximate([OutputGroupsH|OutputGroupsT]) :-
     get_distance(Distance, NotUnique, OutputGroupsT, OutputGroupsH),
     NotUnique #=> Distance #=0,
     approximate(OutputGroupsT).
 
 fill_differences(_,[],[]).
-fill_differences(OutputIndexs, [OutputIndexsH|OutputIndexsT], [DifferencesH|DifferencesT]):-
+fill_differences(OutputIndexs, [OutputIndexsH|OutputIndexsT], [DifferencesH|DifferencesT]) :-
     element(OutputPos, OutputIndexs, OutputIndexsH),
     DifferencesH #= abs(OutputPos-OutputIndexsH),
     fill_differences(OutputIndexs, OutputIndexsT, DifferencesT).
 
 get_changes(Counter, Differences) :-
     changes_signature(Differences, Sign),
-    automaton(Sign,_,Sign,
+    automaton(Sign, _, Sign,
         [source(i), sink(i)],
         [arc(i,0,i,[C+0]), arc(i,1,i, [C+1])],
         [C], [0], [Counter]).    
 
 changes_signature([],[]).
-changes_signature([X|Xs], [S|Ss]):-
+changes_signature([X|Xs], [S|Ss]) :-
     S in 0..1,
     X#\=0 #=> S#=1,
     X#=0 #=> S#=0,
